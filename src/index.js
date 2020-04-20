6b5dfede0a85fa159c6c6474067e6793fc0ac539
Haemin <fcit13@naver.com>
2020-04-20 12:55:10 +0900

    test

--- src/index.js
+++ src/index.js
@@ -4,41 +4,75 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-class Clock extends React.Component {
+class LoginControl extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {date: new Date()};
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    this.state = {isLoggedIn: false};
   }
 
-  componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),
-      1000
-    );
+  handleLoginClick() {
+    this.setState({isLoggedIn: true});
   }
 
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-
-  tick() {
-    this.setState({
-      date: new Date()
-    });
+  handleLogoutClick() {
+    this.setState({isLoggedIn: false});
   }
 
   render() {
+    const isLoggedIn = this.state.isLoggedIn;
+    let button;
+
+    if (isLoggedIn) {
+      button = <LogoutButton onClick={this.handleLogoutClick} />;
+    } else {
+      button = <LoginButton onClick={this.handleLoginClick} />;
+    }
+
     return (
       <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        <Greeting isLoggedIn={isLoggedIn} />
+        {button}
       </div>
     );
   }
 }
 
+function UserGreeting(props) {
+  return <h1>Welcome back!</h1>;
+}
+
+function GuestGreeting(props) {
+  return <h1>Please sign up.</h1>;
+}
+
+function Greeting(props) {
+  const isLoggedIn = props.isLoggedIn;
+  if (isLoggedIn) {
+    return <UserGreeting />;
+  }
+  return <GuestGreeting />;
+}
+
+function LoginButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Login
+    </button>
+  );
+}
+
+function LogoutButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Logout
+    </button>
+  );
+}
+
 ReactDOM.render(
-  <Clock />,
+  <LoginControl />,
   document.getElementById('root')
 );
 
